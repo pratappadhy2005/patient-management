7f0c0844bf0e7e0080a176aa0207163c7aef7fb1
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-02 19:11:16 +1000

    -Added swagger documentation and removed .idea directory

--- patient-service/src/main/java/au/com/pratap/controller/PatientController.java
+++ patient-service/src/main/java/au/com/pratap/controller/PatientController.java
@@ -4,7 +4,8 @@ import au.com.pratap.dto.PatientRequestDTO;
 import au.com.pratap.dto.PatientResponseDTO;
 import au.com.pratap.dto.validators.CreatePatientValidationGroup;
 import au.com.pratap.service.PatientService;
-import jakarta.validation.Valid;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.groups.Default;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -15,6 +16,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients")
+@Tag(name = "Patient Management", description = "APIs for managing patients")
 public class PatientController {
 
     private final PatientService patientService;
@@ -29,6 +31,7 @@ public class PatientController {
      * @return a list of PatientResponseDTO containing patient details.
      */
     @GetMapping
+    @Operation(summary = "Get all patients", description = "Retrieves a list of all patients in the system.")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients() {
         List<PatientResponseDTO> patients = patientService.getPatients();
         return ResponseEntity.ok().body(patients);
@@ -41,6 +44,7 @@ public class PatientController {
      * @return a response indicating that the method is not implemented.
      */
     @PostMapping
+    @Operation(summary = "Create a new patient", description = "Saves a new patient in the system.")
     public ResponseEntity<PatientResponseDTO> createPatient(@Validated({Default.class,
             CreatePatientValidationGroup.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
         final PatientResponseDTO patientResponseDTO = patientService.savePatient(patientRequestDTO);
@@ -51,11 +55,12 @@ public class PatientController {
      * Endpoint to update an existing patient.
      * This method is not implemented yet.
      *
-     * @param id the ID of the patient to update
+     * @param id                the ID of the patient to update
      * @param patientRequestDTO the updated patient details
      * @return a response indicating that the method is not implemented.
      */
     @PutMapping("/{id}")
+    @Operation(summary = "Update an existing patient", description = "Updates the details of an existing patient.")
     public ResponseEntity<PatientResponseDTO> updatePatient(
             @PathVariable("id") UUID id,
             @Validated({Default.class}) @RequestBody PatientRequestDTO patientRequestDTO) {
@@ -70,6 +75,7 @@ public class PatientController {
      * @return a response indicating that the patient was deleted successfully.
      */
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a patient", description = "Deletes a patient by ID.")
     public ResponseEntity<Void> deletePatient(@PathVariable("id") UUID id) {
         patientService.deletePatient(id);
         return ResponseEntity.noContent().build();
