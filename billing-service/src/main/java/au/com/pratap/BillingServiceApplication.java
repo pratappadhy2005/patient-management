f6ffe650d2a544638e6c682982f67703719434e9
Pratap Padhy <pratappadhy2005@gmail.com>
2025-06-04 18:30:47 +1000

    -Added billing service skeleton with gRPC

---
+++ billing-service/src/main/java/au/com/pratap/BillingServiceApplication.java
@@ -0,0 +1,13 @@
+package au.com.pratap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingServiceApplication.class, args);
+	}
+
+}
